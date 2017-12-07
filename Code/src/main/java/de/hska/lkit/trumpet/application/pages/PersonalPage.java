854a030cdfd3a292d11f9e9b07e00ed6626319d2
Fabian Sorn <fabian.sorn@online.de>
2017-12-07 20:06:15 +0100

    Ajax Request and Controller for Searching for an User

--- Code/src/main/java/de/hska/lkit/trumpet/application/pages/PersonalPage.java
+++ Code/src/main/java/de/hska/lkit/trumpet/application/pages/PersonalPage.java
@@ -10,7 +10,6 @@ public class PersonalPage {
    
 	ServiceBundle service;
 	public List<Tweet> personalTweets;
-	public String searchExpression;
 	public List<User> searchResult;
 	public User user;
 
@@ -23,27 +22,12 @@ public class PersonalPage {
 		this.user.updateSubscriptionsList();
 		this.updateTweetList();
 	}
-	
-	public void searchForUser() {
-		System.out.println(this.searchExpression);
-		this.service.searchForUsersByExpression(this.searchExpression).ifPresent( users -> {
-			this.searchResult = users;
-		});
-	}
 
 	private void updateTweetList() {
 		this.service.getPersonalTweets(this.user).ifPresent( tweets -> {
 			System.out.println("[Global-Page-Info]: There were found " + tweets.size() + " Tweets on the global list.");
 			this.personalTweets = tweets;
 		});
-	}
-	
-	public String getSearchExpression() {
-		return this.searchExpression;
-	}
-	
-	public void setSearchExpression(String searchExpression) {
-		this.searchExpression = searchExpression;
 	}
 	
 }
