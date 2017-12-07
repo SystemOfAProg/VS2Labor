ca91ec295d254b653c3c776f2ba64e93209a4553
Fabian Sorn <fabian.sorn@online.de>
2017-12-07 20:45:07 +0100

    User Search For Global and Subscription Page

--- Code/src/main/resources/static/js/searchResultHandler.js
+++ Code/src/main/resources/static/js/searchResultHandler.js
@@ -1,4 +1,4 @@
-function searchForUsers() {
+function searchForUsersProfile() {
 	var searchExpression = $('#searchFieldInput').val();
 	if(searchExpression){
 		var requestURL = 'http://localhost:8080/searchUser?expression=' + escape(searchExpression);
@@ -8,7 +8,7 @@ function searchForUsers() {
 	        url: requestURL, 
 	        data: { get_param: 'value' }, 
 	        success: function (data) { 
-	        	addResponseToTemplate(data);
+	        	addResponseToTemplateProfile(data);
 	        }
 	    });
 	} else {
@@ -16,7 +16,7 @@ function searchForUsers() {
 	}
 }
 
-function addResponseToTemplate(response){
+function addResponseToTemplateProfile(response){
 	var searchResultBody = $('#searchResultDialogBody');
 	searchResultBody.html("");
 	var template = '';
@@ -25,7 +25,7 @@ function addResponseToTemplate(response){
 		var name = user['name'];
 		var tagname = user['tagName'];
 		if(name && tagname){
-			var templatePart = buildTemplateFrom(name, tagname);
+			var templatePart = buildTemplateFromProfile(name, tagname);
 			if(templatePart) {
 				template += templatePart;
 			}
@@ -34,9 +34,66 @@ function addResponseToTemplate(response){
 	searchResultBody.html(template);
 }
 
-function buildTemplateFrom(name, tagname) {
+function buildTemplateFromProfile(name, tagname) {
     var template = '';
     template += '<a href="'+ escape(name.trim()) +'">';
+    template += '	<div class="userListElement">';
+    template += '		<div class="listProfileImage">';
+    template += '			<img src="/images/profileImage.jpg" class="listProfileImage"/>';
+    template += '		</div>';
+    template += '		<div class="listTextWrapper">';
+    template += '			<p class="listFullName">' + name + '</p>';
+    template += '		</div>';
+    template += '		<div class="listTextWrapper">';
+    template += '			<p class="listTagName">' + tagname + '</p>';
+    template += '		</div>';
+    template += '		<div class="listTextWrapper">';
+    template += '			<i class="material-icons">keyboard_arrow_right</i>';
+    template += '		</div>';
+    template += '	</div>';
+    template += '</a>';
+	return template;
+}
+
+function searchForUsersNonProfile() {
+	var searchExpression = $('#searchFieldInput').val();
+	if(searchExpression){
+		var requestURL = 'http://localhost:8080/searchUser?expression=' + escape(searchExpression);
+		console.log(requestURL);
+		$.ajax({ 
+	        type: 'GET', 
+	        url: requestURL, 
+	        data: { get_param: 'value' }, 
+	        success: function (data) { 
+	        	addResponseToTemplateNonProfile(data);
+	        }
+	    });
+	} else {
+		console.warn("Value of Searchfield could not be read.");
+	}
+}
+
+function addResponseToTemplateNonProfile(response){
+	var searchResultBody = $('#searchResultDialogBody');
+	searchResultBody.html("");
+	var template = '';
+	for(var user of response){
+		console.log(user);
+		var name = user['name'];
+		var tagname = user['tagName'];
+		if(name && tagname){
+			var templatePart = buildTemplateFromNonProfile(name, tagname);
+			if(templatePart) {
+				template += templatePart;
+			}
+		}
+	}
+	searchResultBody.html(template);
+}
+
+function buildTemplateFromNonProfile(name, tagname) {
+    var template = '';
+    template += '<a href="profile/'+ escape(name.trim()) +'">';
     template += '	<div class="userListElement">';
     template += '		<div class="listProfileImage">';
     template += '			<img src="/images/profileImage.jpg" class="listProfileImage"/>';
